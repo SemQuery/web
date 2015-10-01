9b91b71c0a135476dd156c85035dff4172318a9d
August <augustt198@gmail.com>
2015-10-01 00:09:50 -0400

    Add job queueing interface

---
+++ app/query/job.go
@@ -0,0 +1,44 @@
+package query
+
+import (
+    "github.com/semquery/web/app/common"
+
+    "log"
+    "encoding/json"
+    "github.com/aws/aws-sdk-go/service/sqs"
+)
+
+type IndexingJob struct {
+    User common.User
+
+    RepositoryOwner string
+    RepositoryName  string
+}
+
+func (j *IndexingJob) toSQSJson() string {
+    data := map[string]interface{}{
+        "user_id": "TODO",
+        "repo_owner": j.RepositoryOwner,
+        "repo_name": j.RepositoryName,
+    }
+
+    encoded, _ := json.Marshal(data)
+
+    return string(encoded)
+}
+
+// returns: whether queueing the job was successful
+func QueueIndexingJob(job IndexingJob) bool {
+    msg := job.toSQSJson()
+    input := sqs.SendMessageInput{
+        MessageBody: &msg,
+        QueueURL: &common.QueueURL,
+    }
+
+    _, err := common.Queue.SendMessage(&input)
+
+    if err != nil {
+        log.Print(err)
+    }
+    return err == nil
+}

09958152a5e52bd4e14b1d8838c96b96176ef4c9
August <augustt198@gmail.com>
2015-12-08 18:07:53 -0500

    Add theme data

--- app/home/home.go
+++ app/home/home.go
@@ -11,6 +11,7 @@ func HomePage(user common.User, r render.Render) {
     data := struct {
         common.User
         Pagename string
-    } {user, "home"}
+        Theme    string
+    } {user, "home", "home"}
     r.HTML(200, "index", data)
 }

747da2d3b7acccadde349feff0724cbe4652d0d9
August <augustt198@gmail.com>
2015-12-17 17:07:29 -0500

    Add function for signing S3 requests

--- app/query/remote.go
+++ app/query/remote.go
@@ -3,10 +3,20 @@ package query
 import (
     "github.com/semquery/web/app/common"
 
+    "fmt"
     "net/url"
     "net/http"
     "encoding/json"
+
+    "github.com/aws/aws-sdk-go/aws/request"
+    "github.com/aws/aws-sdk-go/service/s3"
 )
+
+// files are split into 50KB sectors
+const FILE_SECTOR_BYTES = 50000
+
+// [ID]/[path]$[start]$[end]
+const FILENAME_FORMAT = "%s/%s$%d$%d"
 
 // Handles querying over HTTP
 
@@ -34,4 +44,28 @@ func ExecuteQuery(query, sourceID string) ([]QueryResult, error) {
     }
 
     return results, nil
+}
+
+// Creates & signs the necessary GET Object requests to
+// fetch the file sections associated with a query result.
+func CreateS3Requests(qr QueryResult, id string) (reqs []*request.Request, err error) {
+    reqs =  []*request.Request{}
+
+    byteStart := FILE_SECTOR_BYTES * (qr.Start / FILE_SECTOR_BYTES)
+    for byteStart <= qr.End {
+        key := fmt.Sprintf(FILENAME_FORMAT, id, qr.File, byteStart, byteStart + FILE_SECTOR_BYTES)
+
+        input := s3.GetObjectInput{
+            Bucket: &common.Config.S3SourceCodeBucket,
+            Key: &key,
+        }
+        req, _ := common.S3SourceCode.GetObjectRequest(&input)
+        err = req.Sign()
+        if err != nil { return }
+        reqs = append(reqs, req)
+
+        byteStart += FILE_SECTOR_BYTES
+    }
+
+    return
 }
